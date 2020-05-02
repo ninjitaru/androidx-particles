c970f969e92e5a19d28ac220c14f161ba7b8aa96
Thomas Orlando <thomorl@atpc.one>
2020-05-02 21:05:06 +0200

    Add ParticleModifier parameter documentation
    Added documentation for the parameters of ParticleModifier.apply().

--- lib/src/main/java/androidx/particles/modifiers/ParticleModifier.java
+++ lib/src/main/java/androidx/particles/modifiers/ParticleModifier.java
@@ -7,8 +7,8 @@ public interface ParticleModifier {
 	/**
 	 * Modifies the specific value of a particle given the current milliseconds.
 	 *
-	 * @param particle
-	 * @param miliseconds
+	 * @param particle The particle to be modified.
+	 * @param miliseconds The current number of milliseconds.
 	 */
 	void apply(Particle particle, long miliseconds);
 
